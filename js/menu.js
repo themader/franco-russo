275b141c96d6603c91bc6e969bc439a93ed88dae
Franco Russo <tomasfrancorusso@gmail.com>
2025-09-24 17:29:24 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+
+    const toggleBtn = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    toggleBtn.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+    });
